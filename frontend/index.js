2f254ee4f74a552c7ac60025968e0476998a790e
Ubuntu <ubuntu@ip-172-31-27-190.eu-north-1.compute.internal>
2026-02-25 06:26:00 +0000

    fixed minor bugs

--- frontend/index.js
+++ frontend/index.js
@@ -4,9 +4,9 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 3001;
 const HOST = '0.0.0.0'
-const BACKEND_URL = process.env.BACKEND_URL || 'http://localhost:8000';
+const BACKEND_URL = 'http://backend-host:8000';
 
 // Set view engine
 app.set('view engine', 'ejs');
